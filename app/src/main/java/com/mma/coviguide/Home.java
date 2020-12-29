2ae2f871d090025dd2a321933713326e7a569fca
MaryemBenMohamed <maryoumabac18@gmail.com>
2020-12-29 12:36:38 +0100

    commit add intents

--- app/src/main/java/com/mma/coviguide/Home.java
+++ app/src/main/java/com/mma/coviguide/Home.java
@@ -1,14 +1,41 @@
 package com.mma.coviguide;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.GridLayout;
+import android.widget.Toast;
+
 
 public class Home extends AppCompatActivity {
+    GridLayout mainGrid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        mainGrid = (GridLayout) findViewById(R.id.mainGrid);
+        setSingleEvent(mainGrid);
+    }
+
+    private void setSingleEvent(GridLayout mainGrid) {
+        for (int i=0;i<mainGrid.getChildCount();i++){
+            CardView cardView = (CardView)mainGrid.getChildAt(i);
+            final int finalI = i;
+            cardView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (finalI == 0){
+                        Intent intent = new Intent(Home.this,AboutVirus.class);
+                        startActivity(intent);
+                        Toast.makeText(Home.this,"Welcom",Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+            });
+        }
     }
 }
