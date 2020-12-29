9a82ef5bf02e38d15dcac07e5434ff1d98f4a186
asma932 <benbrahime149@gmail.com>
2020-12-29 23:25:13 +0100

    links

--- app/src/main/java/com/mma/coviguide/Login.java
+++ app/src/main/java/com/mma/coviguide/Login.java
@@ -2,13 +2,26 @@ package com.mma.coviguide;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
-
+    public Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-    }
-}
+
+        button = (Button) findViewById(R.id.btn1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this,Home.class);
+                startActivity(intent);
+                Toast.makeText(Login.this,"Welcom",Toast.LENGTH_SHORT).show();
+            }
+
+        });}}
--- app/src/main/java/com/mma/coviguide/MainActivity.java
+++ app/src/main/java/com/mma/coviguide/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,Home.class);
+                Intent intent = new Intent(MainActivity.this,SignUp.class);
                 startActivity(intent);
                 Toast.makeText(MainActivity.this,"Welcom",Toast.LENGTH_SHORT).show();
             }
