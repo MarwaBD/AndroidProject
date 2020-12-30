f21807fd1e6cd66dec3538f697b95ef0f16db098
MarwaBD <marwa1998bendaoud@gmail.com>
2020-12-30 12:02:10 +0100

    fixe bugs

--- app/src/main/java/com/mma/coviguide/AboutVirus.java
+++ app/src/main/java/com/mma/coviguide/AboutVirus.java
@@ -3,6 +3,9 @@ package com.mma.coviguide;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class AboutVirus extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class AboutVirus extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_virus);
+    }
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch ( item.getItemId() ){
+            case R.id.menu_home:
+                Toast.makeText(this, "Home", Toast.LENGTH_SHORT).show();
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+
+
     }
 }
