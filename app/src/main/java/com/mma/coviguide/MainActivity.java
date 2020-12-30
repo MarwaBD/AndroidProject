5f29455ea980ce5fbb83923aff059afef07908bc
MarwaBD <marwa1998bendaoud@gmail.com>
2020-12-30 17:03:44 +0100

    browser intent,labled activities

--- app/src/main/java/com/mma/coviguide/MainActivity.java
+++ app/src/main/java/com/mma/coviguide/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,SignUp.class);
+                Intent intent = new Intent(MainActivity.this,Home.class);
                 startActivity(intent);
                 Toast.makeText(MainActivity.this,"Welcom",Toast.LENGTH_SHORT).show();
             }
